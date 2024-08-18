7338007a01e5d4387493b2f89c81207b7b390cf1
Wicho <54341712+Wichosu@users.noreply.github.com>
2024-08-18 12:20:49 -0600

    My first attempt to solve the workshop

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,33 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    "--font-size": 14 + "px",
+    "--wrapper-padding": 4 + "px" + " " + 0 + "px",
+    "--input-padding": 8 + "px",
+    "--border-bottom": 1 + "px",
+    "--outline-offset": 2 + "px"
+  },
+  large: {
+    "--font-size": 18 + "px",
+    "--wrapper-padding": 8 + "px" + " " + 0 + "px",
+    "--input-padding": 12 + "px",
+    "--border-bottom": 2 + "px",
+    "--outline-offset": 4 + "px"
+  }
+}
+
+const ICON_SIZES = {
+  small: 16,
+  large: 24,
+}
+
+const ICON_STROKEWIDTH = {
+  small: 1,
+  large: 2,
+}
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +40,64 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  let styles = SIZES[size];
+
+  return (
+    <Wrapper style={{"--wrapper-width": width + "px", ...styles}}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <label htmlFor='input'>
+        <CustomIcon id={icon} size={ICON_SIZES[size]} strokeWidth={ICON_STROKEWIDTH[size]} />
+      </label>
+      <Input id='input' placeholder={placeholder} style={styles} />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  width: var(--wrapper-width);
+  border-bottom: var(--border-bottom) solid ${COLORS.black};
+  background-color: ${COLORS.transparentGray15};
+  position: relative;
+  padding: var(--wrapper-padding);
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const CustomIcon = styled(Icon)`
+  display: inline-block;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const Input = styled.input`
+  background-color: transparent;
+  border: none;
+  color: ${COLORS.gray700};
+  font-size: var(--font-size);
+  font-weight: 700;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  padding-left: var(--input-padding);
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+
+  &:focus {
+    outline: 2px solid ${COLORS.primary};
+    outline-offset: var(--outline-offset);
+  }
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
 
 export default IconInput;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,33 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <label>
+      {label}
+      <CustomSelect style={{"--width": displayedValue.length * 15 + "px"}} value={value} onChange={onChange}>
+        {children}
+      </CustomSelect>
+    </label>
   );
 };
+
+const CustomSelect = styled.select`
+  width: var(--width);
+  max-width: fit-content;
+  border: unset;
+  border-radius: 8px;
+  background-color: ${COLORS.gray50};
+  padding: 12px 16px;
+  margin-left: 12px;
+  font-size: 1rem;
+  color: ${COLORS.gray700};
+
+  &:focus {
+    outline: 2px solid ${COLORS.primary};
+  }
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
 
 export default Select;
